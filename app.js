c1f10634659e61e710c0eb454001e21eb9900207
GS <tonifont2@paucasesnovescifp.cat>
2026-02-16 16:35:03 +0100

    api final

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 
 const pokemonroutes = require('./routes/pokemon-routes');
+
 app.use('/api/pokedex', pokemonroutes);
 app.use('/pokemon', pokemonroutes);
-
+app.use('/pokemon/finales', pokemonroutes);
+app.use('/pokemon', pokemonroutes);
 app.use(express.json());
 app.listen(3000, () => console.log('Servidor en http://localhost:3000'));
--- routes/pokemon-routes.js
+++ routes/pokemon-routes.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 const pokemoncontroller = require('../controllers/pokemon-controllers');
-
 router.get('/pokedex', pokemoncontroller.llistarpokemon);
 const pokemonController = require('../controllers/pokemon-controllers');
 router.get('/', pokemonController.llistarpokemon);
+router.get('/search', pokemonController.getPokemonsByQuery);
 router.get('/:id', pokemonController.getPokemonById);
 module.exports = router;
